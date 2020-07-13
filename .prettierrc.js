e68914edda7356032984a6919cafb836e2790838
q-digital <q-digital.org>
2020-07-13 15:49:06 +0400

    Created theming for buttons

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	bracketSpacing: false,
+	jsxBracketSameLine: true,
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	tabWidth: 2,
+};
---
+++ js/Styles/Base/variables.js
@@ -0,0 +1,17 @@
+export const variables = {
+	colors: {
+		red_dark: '#DF2E2E',
+		red: '#FF5652',
+		orange_light: '#FF8B7B',
+		orange_light_dark: '#ED847E',
+		black: '#000000',
+		grey: '#263238',
+		pink: '#FFC3BD',
+		light_blue: '#4891E5',
+		dark_blue: '#0E3B6E',
+		white: '#FFFFFF',
+		blue: '#2B70BF',
+		biege: '#C3DAF5',
+		biege_dark: '#BFD8F6',
+	},
+};
