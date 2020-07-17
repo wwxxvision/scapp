d426b0b12a47f54d2ac6b32932b6f723ab0d1207
Osoro <as.stolbov@gmail.com>
2020-07-17 14:06:33 +0400

    added proptypes for nav component

--- src/js/Components/Nav.js
+++ src/js/Components/Nav.js
@@ -3,6 +3,7 @@ import { ROUTER } from '../router';
 import { View, Text } from 'react-native';
 import getIconByName from '../Utils/getIconByName';
 import { navStyles } from '../../Styles/Components';
+import PropsTypes from 'prop-types';
 
 export default class Nav extends Component {
 	render() {
@@ -23,3 +24,11 @@ export default class Nav extends Component {
 		);
 	}
 }
+
+Nav.defaultProps = {
+	theme: 'white',
+};
+
+Nav.PropsTypes = {
+	theme: PropsTypes.string,
+};
