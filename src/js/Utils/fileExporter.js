aecb7fc61609c666b2d05c72f06e554e27d04887
Osoro <as.stolbov@gmail.com>
2020-07-22 14:09:30 +0400

    Adding layering input type number

---
+++ src/js/Utils/fileExporter.js
@@ -0,0 +1,3 @@
+const phoneCodeOfCountries = require('../Store/countries.json');
+
+export { phoneCodeOfCountries };
--- src/js/Utils/imageExporter.js
+++ src/js/Utils/imageExporter.js
@@ -26,6 +26,496 @@ import withdrawl from '../../assets/img/withdrawl_icon.svg';
 import supportCall from '../../assets/img/support-call_icon.svg';
 import feedback from '../../assets/img/feedback_icon.svg';
 import logout from '../../assets/img/logout_icon.svg';
+import arrowDown from '../../assets/img/arrow-down_icon.svg';
+import AC from '../../assets/img/flags/AC.svg';
+import AD from '../../assets/img/flags/AD.svg';
+import AE from '../../assets/img/flags/AE.svg';
+import AG from '../../assets/img/flags/AG.svg';
+import AF from '../../assets/img/flags/AF.svg';
+import AM from '../../assets/img/flags/AM.svg';
+import AL from '../../assets/img/flags/AL.svg';
+import AI from '../../assets/img/flags/AI.svg';
+import AO from '../../assets/img/flags/AO.svg';
+import AQ from '../../assets/img/flags/AQ.svg';
+import AR from '../../assets/img/flags/AR.svg';
+
+import AS from '../../assets/img/flags/AS.svg';
+
+import AT from '../../assets/img/flags/AT.svg';
+
+import AU from '../../assets/img/flags/AU.svg';
+
+import AW from '../../assets/img/flags/AW.svg';
+
+import AX from '../../assets/img/flags/AX.svg';
+
+import AZ from '../../assets/img/flags/AZ.svg';
+
+import BA from '../../assets/img/flags/BA.svg';
+
+import BB from '../../assets/img/flags/BB.svg';
+
+import BD from '../../assets/img/flags/BD.svg';
+
+import BE from '../../assets/img/flags/BE.svg';
+
+import BF from '../../assets/img/flags/BF.svg';
+
+import BG from '../../assets/img/flags/BG.svg';
+
+import BH from '../../assets/img/flags/BH.svg';
+
+import BI from '../../assets/img/flags/BI.svg';
+
+import BJ from '../../assets/img/flags/BJ.svg';
+
+import BL from '../../assets/img/flags/BL.svg';
+
+import BM from '../../assets/img/flags/BM.svg';
+
+import BN from '../../assets/img/flags/BN.svg';
+
+import BO from '../../assets/img/flags/BO.svg';
+
+import BQ from '../../assets/img/flags/BQ.svg';
+
+import BR from '../../assets/img/flags/BR.svg';
+
+import BS from '../../assets/img/flags/BS.svg';
+
+import BT from '../../assets/img/flags/BT.svg';
+
+import BV from '../../assets/img/flags/BV.svg';
+
+import BW from '../../assets/img/flags/BW.svg';
+
+import BY from '../../assets/img/flags/BY.svg';
+
+import CA from '../../assets/img/flags/CA.svg';
+
+import BZ from '../../assets/img/flags/BZ.svg';
+
+import CC from '../../assets/img/flags/CC.svg';
+
+import CD from '../../assets/img/flags/CD.svg';
+
+import CF from '../../assets/img/flags/CF.svg';
+
+import CG from '../../assets/img/flags/CG.svg';
+
+import CH from '../../assets/img/flags/CH.svg';
+
+import CI from '../../assets/img/flags/CI.svg';
+
+import CK from '../../assets/img/flags/CK.svg';
+
+import CL from '../../assets/img/flags/CL.svg';
+
+import CM from '../../assets/img/flags/CM.svg';
+
+import CN from '../../assets/img/flags/CN.svg';
+
+import CO from '../../assets/img/flags/CO.svg';
+
+import CR from '../../assets/img/flags/CR.svg';
+
+import CU from '../../assets/img/flags/CU.svg';
+
+import CV from '../../assets/img/flags/CV.svg';
+
+import CW from '../../assets/img/flags/CW.svg';
+
+import CX from '../../assets/img/flags/CX.svg';
+
+import CY from '../../assets/img/flags/CY.svg';
+
+import CZ from '../../assets/img/flags/CZ.svg';
+
+import DE from '../../assets/img/flags/DE.svg';
+
+import DJ from '../../assets/img/flags/DJ.svg';
+
+import DK from '../../assets/img/flags/DK.svg';
+
+import DM from '../../assets/img/flags/DM.svg';
+
+import DO from '../../assets/img/flags/DO.svg';
+
+import DZ from '../../assets/img/flags/DZ.svg';
+
+import EC from '../../assets/img/flags/EC.svg';
+
+import EE from '../../assets/img/flags/EE.svg';
+
+import EG from '../../assets/img/flags/EG.svg';
+
+import EH from '../../assets/img/flags/EH.svg';
+
+import ER from '../../assets/img/flags/ER.svg';
+
+import ES from '../../assets/img/flags/ES.svg';
+
+import ET from '../../assets/img/flags/ET.svg';
+
+import FI from '../../assets/img/flags/FI.svg';
+
+import FJ from '../../assets/img/flags/FJ.svg';
+
+import FK from '../../assets/img/flags/FK.svg';
+
+import FM from '../../assets/img/flags/FM.svg';
+
+import FO from '../../assets/img/flags/FO.svg';
+
+import FR from '../../assets/img/flags/FR.svg';
+
+import GA from '../../assets/img/flags/GA.svg';
+
+import GB from '../../assets/img/flags/GB.svg';
+
+import GD from '../../assets/img/flags/GD.svg';
+
+import GE from '../../assets/img/flags/GE.svg';
+
+import GF from '../../assets/img/flags/GF.svg';
+
+import GG from '../../assets/img/flags/GG.svg';
+
+import GH from '../../assets/img/flags/GH.svg';
+
+import GI from '../../assets/img/flags/GI.svg';
+
+import GL from '../../assets/img/flags/GL.svg';
+
+import GM from '../../assets/img/flags/GM.svg';
+
+import GN from '../../assets/img/flags/GN.svg';
+
+import GP from '../../assets/img/flags/GP.svg';
+
+import GQ from '../../assets/img/flags/GQ.svg';
+
+import GR from '../../assets/img/flags/GR.svg';
+
+import GS from '../../assets/img/flags/GS.svg';
+
+import GT from '../../assets/img/flags/GT.svg';
+
+import GU from '../../assets/img/flags/GU.svg';
+
+import GW from '../../assets/img/flags/GW.svg';
+
+import GY from '../../assets/img/flags/GY.svg';
+
+import HK from '../../assets/img/flags/HK.svg';
+
+import HM from '../../assets/img/flags/HM.svg';
+
+import HN from '../../assets/img/flags/HN.svg';
+
+import HR from '../../assets/img/flags/HR.svg';
+
+import HT from '../../assets/img/flags/HT.svg';
+
+import HU from '../../assets/img/flags/HU.svg';
+
+import ID from '../../assets/img/flags/ID.svg';
+
+import IE from '../../assets/img/flags/IE.svg';
+
+import IL from '../../assets/img/flags/IL.svg';
+
+import IM from '../../assets/img/flags/IM.svg';
+
+import IN from '../../assets/img/flags/IN.svg';
+
+import IO from '../../assets/img/flags/IO.svg';
+
+import IQ from '../../assets/img/flags/IQ.svg';
+
+import IR from '../../assets/img/flags/IR.svg';
+
+import IS from '../../assets/img/flags/IS.svg';
+
+import IT from '../../assets/img/flags/IT.svg';
+
+import JE from '../../assets/img/flags/JE.svg';
+
+import JM from '../../assets/img/flags/JM.svg';
+
+import JO from '../../assets/img/flags/JO.svg';
+
+import JP from '../../assets/img/flags/JP.svg';
+
+import KE from '../../assets/img/flags/KE.svg';
+
+import KG from '../../assets/img/flags/KG.svg';
+
+import KH from '../../assets/img/flags/KH.svg';
+
+import KI from '../../assets/img/flags/KI.svg';
+
+import KM from '../../assets/img/flags/KM.svg';
+
+import KP from '../../assets/img/flags/KP.svg';
+
+import KR from '../../assets/img/flags/KR.svg';
+
+import KN from '../../assets/img/flags/KN.svg';
+
+import KW from '../../assets/img/flags/KW.svg';
+
+import KY from '../../assets/img/flags/KY.svg';
+
+import KZ from '../../assets/img/flags/KZ.svg';
+
+import LA from '../../assets/img/flags/LA.svg';
+
+import LB from '../../assets/img/flags/LB.svg';
+
+import LC from '../../assets/img/flags/LC.svg';
+
+import LI from '../../assets/img/flags/LI.svg';
+
+import LK from '../../assets/img/flags/LK.svg';
+
+import LR from '../../assets/img/flags/LR.svg';
+
+import LT from '../../assets/img/flags/LT.svg';
+
+import LS from '../../assets/img/flags/LS.svg';
+
+import LU from '../../assets/img/flags/LU.svg';
+
+import LV from '../../assets/img/flags/LV.svg';
+
+import LY from '../../assets/img/flags/LY.svg';
+
+import MA from '../../assets/img/flags/MA.svg';
+
+import MC from '../../assets/img/flags/MC.svg';
+
+import MF from '../../assets/img/flags/MF.svg';
+
+import MD from '../../assets/img/flags/MD.svg';
+
+import ME from '../../assets/img/flags/ME.svg';
+
+import MG from '../../assets/img/flags/MG.svg';
+
+import MH from '../../assets/img/flags/MH.svg';
+
+import MK from '../../assets/img/flags/MK.svg';
+
+import ML from '../../assets/img/flags/ML.svg';
+
+import MM from '../../assets/img/flags/MM.svg';
+
+import MN from '../../assets/img/flags/MN.svg';
+
+import MO from '../../assets/img/flags/MO.svg';
+
+import MP from '../../assets/img/flags/MP.svg';
+
+import MQ from '../../assets/img/flags/MQ.svg';
+
+import MR from '../../assets/img/flags/MR.svg';
+
+import MS from '../../assets/img/flags/MS.svg';
+
+import MT from '../../assets/img/flags/MT.svg';
+
+import MU from '../../assets/img/flags/MU.svg';
+
+import MV from '../../assets/img/flags/MV.svg';
+
+import MW from '../../assets/img/flags/MW.svg';
+
+import MX from '../../assets/img/flags/MX.svg';
+
+import MY from '../../assets/img/flags/MY.svg';
+
+import MZ from '../../assets/img/flags/MZ.svg';
+
+import NA from '../../assets/img/flags/NA.svg';
+
+import NC from '../../assets/img/flags/NC.svg';
+
+import NE from '../../assets/img/flags/NE.svg';
+
+import NF from '../../assets/img/flags/NF.svg';
+
+import NG from '../../assets/img/flags/NG.svg';
+
+import NI from '../../assets/img/flags/NI.svg';
+
+import NL from '../../assets/img/flags/NL.svg';
+
+import NO from '../../assets/img/flags/NO.svg';
+
+import NP from '../../assets/img/flags/NP.svg';
+
+import NR from '../../assets/img/flags/NR.svg';
+
+import NU from '../../assets/img/flags/NU.svg';
+
+import NZ from '../../assets/img/flags/NZ.svg';
+
+import OM from '../../assets/img/flags/OM.svg';
+
+import PA from '../../assets/img/flags/PA.svg';
+
+import PF from '../../assets/img/flags/PF.svg';
+
+import PE from '../../assets/img/flags/PE.svg';
+
+import PG from '../../assets/img/flags/PG.svg';
+
+import PK from '../../assets/img/flags/PK.svg';
+
+import PH from '../../assets/img/flags/PH.svg';
+
+import PL from '../../assets/img/flags/PL.svg';
+
+import PN from '../../assets/img/flags/PN.svg';
+
+import PM from '../../assets/img/flags/PM.svg';
+
+import PS from '../../assets/img/flags/PS.svg';
+
+import PR from '../../assets/img/flags/PR.svg';
+
+import PT from '../../assets/img/flags/PT.svg';
+
+import PW from '../../assets/img/flags/PW.svg';
+
+import PY from '../../assets/img/flags/PY.svg';
+
+import QA from '../../assets/img/flags/QA.svg';
+
+import RE from '../../assets/img/flags/RE.svg';
+
+import RO from '../../assets/img/flags/RO.svg';
+
+import RS from '../../assets/img/flags/RS.svg';
+
+import RU from '../../assets/img/flags/RU.svg';
+
+import RW from '../../assets/img/flags/RW.svg';
+
+import SA from '../../assets/img/flags/SA.svg';
+
+import SB from '../../assets/img/flags/SB.svg';
+
+import SC from '../../assets/img/flags/SC.svg';
+
+import SD from '../../assets/img/flags/SD.svg';
+
+import SE from '../../assets/img/flags/SE.svg';
+
+import SG from '../../assets/img/flags/SG.svg';
+
+import SH from '../../assets/img/flags/SH.svg';
+
+import SI from '../../assets/img/flags/SI.svg';
+
+import SJ from '../../assets/img/flags/SJ.svg';
+
+import SK from '../../assets/img/flags/SK.svg';
+
+import SL from '../../assets/img/flags/SL.svg';
+
+import SM from '../../assets/img/flags/SM.svg';
+
+import SN from '../../assets/img/flags/SN.svg';
+
+import SO from '../../assets/img/flags/SO.svg';
+
+import SR from '../../assets/img/flags/SR.svg';
+
+import SS from '../../assets/img/flags/SS.svg';
+
+import ST from '../../assets/img/flags/ST.svg';
+
+import SV from '../../assets/img/flags/SV.svg';
+
+import SX from '../../assets/img/flags/SX.svg';
+
+import SY from '../../assets/img/flags/SY.svg';
+
+import TA from '../../assets/img/flags/TA.svg';
+
+import SZ from '../../assets/img/flags/SZ.svg';
+
+import TC from '../../assets/img/flags/TC.svg';
+
+import TD from '../../assets/img/flags/TD.svg';
+
+import TG from '../../assets/img/flags/TG.svg';
+
+import TH from '../../assets/img/flags/TH.svg';
+
+import TJ from '../../assets/img/flags/TJ.svg';
+
+import TK from '../../assets/img/flags/TK.svg';
+
+import TL from '../../assets/img/flags/TL.svg';
+
+import TM from '../../assets/img/flags/TM.svg';
+
+import TN from '../../assets/img/flags/TN.svg';
+
+import TO from '../../assets/img/flags/TO.svg';
+
+import TR from '../../assets/img/flags/TR.svg';
+
+import TT from '../../assets/img/flags/TT.svg';
+
+import TV from '../../assets/img/flags/TV.svg';
+
+import TW from '../../assets/img/flags/TW.svg';
+
+import TZ from '../../assets/img/flags/TZ.svg';
+
+import UA from '../../assets/img/flags/UA.svg';
+
+import UG from '../../assets/img/flags/UG.svg';
+
+import US from '../../assets/img/flags/US.svg';
+
+import UY from '../../assets/img/flags/UY.svg';
+
+import UZ from '../../assets/img/flags/UZ.svg';
+
+import VA from '../../assets/img/flags/VA.svg';
+
+import VC from '../../assets/img/flags/VC.svg';
+
+import VE from '../../assets/img/flags/VE.svg';
+
+import VG from '../../assets/img/flags/VG.svg';
+
+import VI from '../../assets/img/flags/VI.svg';
+
+import VN from '../../assets/img/flags/VN.svg';
+
+import WF from '../../assets/img/flags/WF.svg';
+
+import VU from '../../assets/img/flags/VU.svg';
+
+import WS from '../../assets/img/flags/WS.svg';
+
+import XK from '../../assets/img/flags/XK.svg';
+
+import YE from '../../assets/img/flags/YE.svg';
+
+import YT from '../../assets/img/flags/YT.svg';
+
+import ZA from '../../assets/img/flags/ZA.svg';
+
+import ZM from '../../assets/img/flags/ZM.svg';
+
+import ZW from '../../assets/img/flags/ZW.svg';
 
 export default {
 	user,
@@ -56,4 +546,505 @@ export default {
 	supportCall,
 	feedback,
 	logout,
+
+	AD,
+
+	AC,
+
+	AF,
+
+	AE,
+
+	AG,
+
+	AI,
+
+	AL,
+
+	AM,
+
+	AO,
+
+	AQ,
+
+	AR,
+
+	AS,
+
+	AT,
+
+	AU,
+
+	AW,
+
+	AX,
+
+	AZ,
+
+	BA,
+
+	BB,
+
+	BD,
+
+	BE,
+
+	BF,
+
+	BG,
+
+	BH,
+
+	BI,
+
+	BJ,
+
+	BL,
+
+	BM,
+
+	BN,
+
+	BO,
+
+	BQ,
+
+	BR,
+
+	BS,
+
+	BT,
+
+	BV,
+
+	BW,
+
+	BY,
+
+	BZ,
+
+	CA,
+
+	CC,
+
+	CD,
+
+	CF,
+
+	CG,
+
+	CH,
+
+	CI,
+
+	CK,
+
+	CL,
+
+	CM,
+
+	CN,
+
+	CU,
+
+	CR,
+
+	CO,
+
+	CV,
+
+	CW,
+
+	CX,
+
+	CY,
+
+	CZ,
+
+	DE,
+
+	DJ,
+
+	DK,
+
+	DM,
+
+	DO,
+
+	DZ,
+
+	EC,
+
+	EE,
+
+	EG,
+
+	EH,
+
+	ER,
+
+	ES,
+
+	ET,
+
+	FJ,
+
+	FI,
+
+	FK,
+
+	FO,
+
+	FR,
+
+	FM,
+
+	GA,
+
+	GD,
+
+	GB,
+
+	GE,
+
+	GF,
+
+	GH,
+
+	GG,
+
+	GI,
+
+	GM,
+
+	GL,
+
+	GN,
+
+	GQ,
+
+	GP,
+
+	GS,
+
+	GR,
+
+	GT,
+
+	GU,
+
+	GW,
+
+	GY,
+
+	HM,
+
+	HK,
+
+	HN,
+
+	HT,
+
+	HR,
+
+	HU,
+
+	IE,
+
+	ID,
+
+	IL,
+
+	IM,
+
+	IN,
+
+	IO,
+
+	IQ,
+
+	IR,
+
+	IS,
+
+	IT,
+
+	JE,
+
+	JM,
+
+	JO,
+
+	JP,
+
+	KE,
+
+	KG,
+
+	KH,
+
+	KI,
+
+	KM,
+
+	KN,
+
+	KP,
+
+	KR,
+
+	KW,
+
+	KY,
+
+	KZ,
+
+	LA,
+
+	LB,
+
+	LC,
+
+	LI,
+
+	LK,
+
+	LR,
+
+	LS,
+
+	LT,
+
+	LU,
+
+	LV,
+
+	LY,
+
+	MA,
+
+	MC,
+
+	MD,
+
+	ME,
+
+	MF,
+
+	MG,
+
+	MH,
+
+	MK,
+
+	ML,
+
+	MM,
+
+	MN,
+
+	MO,
+
+	MP,
+
+	MQ,
+
+	MR,
+
+	MS,
+
+	MT,
+
+	MU,
+
+	MV,
+
+	MW,
+
+	MX,
+
+	MY,
+
+	MZ,
+
+	NA,
+
+	NC,
+
+	NE,
+
+	NF,
+
+	NG,
+
+	NI,
+
+	NL,
+
+	NO,
+
+	NP,
+
+	NR,
+
+	NU,
+
+	NZ,
+
+	OM,
+
+	PA,
+
+	PE,
+
+	PF,
+
+	PG,
+
+	PH,
+
+	PK,
+
+	PL,
+
+	PM,
+
+	PN,
+
+	PR,
+
+	PS,
+
+	PT,
+
+	PW,
+
+	PY,
+
+	QA,
+
+	RE,
+
+	RO,
+
+	RS,
+
+	RU,
+
+	RW,
+
+	SA,
+
+	SB,
+
+	SC,
+
+	SD,
+
+	SE,
+
+	SG,
+
+	SH,
+
+	SI,
+
+	SJ,
+
+	SK,
+
+	SL,
+
+	SM,
+
+	SN,
+
+	SO,
+
+	SR,
+
+	SS,
+
+	ST,
+
+	SV,
+
+	SX,
+
+	SY,
+
+	SZ,
+
+	TA,
+
+	TC,
+
+	TD,
+
+	TG,
+
+	TH,
+
+	TJ,
+
+	TK,
+
+	TL,
+
+	TM,
+
+	TN,
+
+	TO,
+
+	TR,
+
+	TT,
+
+	TV,
+
+	TW,
+
+	TZ,
+
+	UA,
+
+	UG,
+
+	US,
+
+	UY,
+
+	UZ,
+
+	VA,
+
+	VC,
+
+	VE,
+
+	VG,
+
+	VI,
+
+	VN,
+
+	WF,
+
+	VU,
+
+	WS,
+
+	YE,
+
+	XK,
+
+	YT,
+
+	ZA,
+
+	ZM,
+
+	ZW,
+	arrowDown,
 };
